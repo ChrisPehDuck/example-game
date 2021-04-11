65e96b149fd6ceb874275a14a9d5b5bb93d9b68f
ChrisPehDuck <82317791+ChrisPehDuck@users.noreply.github.com>
2021-04-12 00:28:07 +0100

    movement

--- Unity Games/HowToMakeAVideoGame/Assets/PlayerMovement.cs
+++ Unity Games/HowToMakeAVideoGame/Assets/PlayerMovement.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Hello, World!");
-    }
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        //only executed if condition is met
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+        }
     }
 }
