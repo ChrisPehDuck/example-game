838bccf984c1489b53c1c8edf504c72706149dd6
ChrisPehDuck <82317791+ChrisPehDuck@users.noreply.github.com>
2021-04-18 22:29:57 +0100

    collision
    new

--- Unity Games/HowToMakeAVideoGame/Assets/PlayerMovement.cs
+++ Unity Games/HowToMakeAVideoGame/Assets/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
 
     public float forwardForce = 2000f;
-    public float sideForce = 500f;
+    public float sidewaysForce = 500f;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -17,11 +17,11 @@ public class PlayerMovement : MonoBehaviour
         //only executed if condition is met
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
         }
     }
 }
